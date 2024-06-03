a8e29a3aa1888acd1f32707458def14d92e287cf
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-06-03 17:21:49 +0530

    save changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom"
-import Exchanges from './components/Exchanges';
-import Coins from './components/Coins';
-import CoinDetails from './components/CoinDetails';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Exchanges from "./components/Exchanges";
+import Coins from "./components/Coins";
+import CoinDetails from "./components/CoinDetails";
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Exchanges/>}  /> 
-      <Route path='/coins' element={<Coins/>}/>
-      <Route path='/coins/:id' element={<CoinDetails/>} />
+      <Route path="/" element={<Coins />} />
+      <Route path="/coins/:id" element={<CoinDetails />} />
     </Routes>
   );
 }
